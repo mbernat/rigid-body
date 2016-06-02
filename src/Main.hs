d1003d0c81639fef73c89b6d1a0ca0f9c9aceca5
Marek Bernat <marek.bernat@gmail.com>
2016-06-03 00:15:22 +0200

    Draw a rectangle

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import SDL.Video
+import SDL.Video (createWindow, defaultWindow, createRenderer)
+import SDL.Video.Renderer
 import SDL.Event
-import SDL.Init
+import SDL.Init (initializeAll)
 import SDL.Input
 import SDL (($=))
-import Linear (V4(..))
+import Linear (V4(..), V2(..))
+import Linear.Affine (Point(..))
 import Control.Monad (unless)
 
 main :: IO ()
@@ -15,6 +17,9 @@ main = do
     window <- createWindow "My SDL Application" defaultWindow
     renderer <- createRenderer window (-1) defaultRenderer
     appLoop renderer
+
+makeRect :: (Num a) => Point V2 a -> a -> Rectangle a
+makeRect (P (V2 x y)) h = Rectangle (P $ V2 (x - h) (y - h)) (V2 h h)
 
 appLoop :: Renderer -> IO ()
 appLoop renderer = do
@@ -26,7 +31,12 @@ appLoop renderer = do
                     keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
                 _ -> False
         qPressed = not (null (filter eventIsQPress events))
-    rendererDrawColor renderer $= V4 0 0 255 255
+
+    rendererDrawColor renderer $= V4 0 0 0 255
     clear renderer
+
+    rendererDrawColor renderer $= V4 255 255 255 255
+    fillRect renderer $ Just $ makeRect (P (V2 200 200)) 100
+
     present renderer
     unless qPressed (appLoop renderer)
